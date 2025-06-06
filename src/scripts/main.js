ee0efd65e4705cb4fa2b59409e09dd6b4122d3d9
SaridDev <saridmf.dev@gmail.com>
2025-06-05 21:49:37 -0600

    Primera version del login

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,8 +3,15 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("visible");
+    } else {
+      entry.target.classList.remove("visible");
     }
   });
 });
 
 document.querySelectorAll(".fade-in").forEach((el) => observer.observe(el));
+
+
+
+
+
